c7634bad54358f44a494121212b4f1e1c2b82c6f
Trigun Joshi <trigunjoshi@gmail.com>
2022-02-28 23:44:41 -0800

    register and login completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import Product from "./pages/Product";
+import Login from "./pages/Login";
+//import Register from "./pages/Register";
+//import Product from "./pages/Product";
 //import Home from "./pages/Home";
 //import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div className="App">
-      <Product />
+      <Login />
     </div>
   );
 }
