248bb57628ec40209b42b5a0b1f90c508f04d9c4
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-01 18:12:40 -0800

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-//import Home from "./pages/Home";
+import Home from "./pages/Home";
 //import Login from "./pages/Login";
-import Register from "./pages/Register";
+//import Register from "./pages/Register";
 //import Product from "./pages/Product";
 //import ProductList from "./pages/ProductList";
 //import Cart from "./pages/Cart";
@@ -9,7 +9,7 @@ import Register from "./pages/Register";
 function App() {
   return (
     <>
-      <Register />
+      <Home />
     </>
   );
 }
