6a1bf023a5ea741ac4f9fe0fd61da5953567d3c4
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-03 17:38:57 -0800

    models added

--- express-back-end/routes/user.js
+++ express-back-end/routes/user.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+module.exports = router;
+// router.get("/usertest", (req, res) => {
+//   res.send("test success")
+// });
 
-router.get("/usertest", (req, res) => {
-  res.send("test success")
-});
-
-router.post("/userposttest", (req, res) => {
-  const username = req.body.username;
-  console.log(username)
-  res.send(`Your username is ${username}`)
-})
+// router.post("/userposttest", (req, res) => {
+//   const username = req.body.username;
+//   console.log(username)
+//   res.send(`Your username is ${username}`)
+// })
 
 
 module.exports = router;  
