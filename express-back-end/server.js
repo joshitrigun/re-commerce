9b3ee3b14a432726a0c2d6ceb8f3b93249c0e856
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-03 17:15:31 -0800

    setting router

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -3,19 +3,23 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 8848;
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+
 
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB connection successful"))
   .catch((err) => {
     console.log(err)
   })
+app.use(express.json());
+
+app.use("/api/users", userRoute);
 
 
-app.get("/api/test", () => {
-  console.log("testing success")
-})
+
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`)
